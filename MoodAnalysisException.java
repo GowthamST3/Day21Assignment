0dda0bfca2ca2fe367c8e1500242ef9bbbbdc540
ST <gowtham.st@dxc.com>
2022-05-21 08:55:06 +0530

    Added - Code to Analyse the mood of the person is happy or sad and Handled using Exception

---
+++ MoodAnalysisException.java
@@ -0,0 +1,10 @@
+package com.BridgeLabz_Day21Assignment;
+
+public class MoodAnalysisException extends Exception{
+	String message;
+
+public MoodAnalysisException(String message) {
+	super(message);
+}
+
+}
